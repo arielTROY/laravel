5002bfa1525e7fc42fbec37c64dc1971a09c80dd
arielT <59286427+arielTROY@users.noreply.github.com>
2023-03-27 14:28:32 +0300

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,7 +7,22 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
-class Controller extends BaseController
+public function store(Request $request)
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    $validatedData = $request->validate([
+        'fullname' => 'required|string',
+        'cv_file' => 'required|file',
+        'image_file' => 'required|file',
+    ]);
+    
+    $cv_file = $request->file('cv_file')->store('public/cv_files');
+    $image_file = $request->file('image_file')->store('public/images');
+    
+    $userData = new UserData();
+    $userData->fullname = $validatedData['fullname'];
+    $userData->cv_file = $cv_file;
+    $userData->image_file = $image_file;
+    $userData->save();
+    
+    return redirect('/')->with('success', 'Data submitted successfully!');
 }
